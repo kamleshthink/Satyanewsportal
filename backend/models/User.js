0157268c661d75dc5e6494ffa838bd5f27e7962e
KAMLESH KUMAR SHARMA <kamleshsamudih2003@gmail.com>
2025-05-24 22:28:04 +0530

    Updated User model collection name to kamleshthink

--- backend/models/User.js
+++ backend/models/User.js
@@ -38,6 +38,6 @@ userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema, 'kamleshthink');
 
 export default User; 
